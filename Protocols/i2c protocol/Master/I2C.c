e641618b34fb9a2318851905ed35e3008a4a460e
Salma Mahmoud <77031899+SaLmaaMahmoud@users.noreply.github.com>
2021-08-12 03:37:43 +0200

    Add files via upload

---
+++ Protocols/i2c protocol/Master/I2C.c
@@ -0,0 +1,103 @@
+﻿/*
+ * I2C.c
+ *
+ * Created: 12/04/2021 04:24:20 PM
+ *  Author: Salma
+ */ 
+
+#include "I2C.h"
+#include "BITMATH.h"
+
+/* Initialize I2C */
+void i2c_voidInit(I2C_PRESCALER pre)
+{
+	//Enable I2C
+	SET_BIT(TWCR , TWEN) ;			
+	
+	//Bit Rate
+	TWBR = 0X48 ;		
+				
+	CLEAR_REG(TWSR) ;	
+	
+	switch ( pre )
+	{
+		case I2C_Prescaler_1 :
+			break;
+		case I2C_Prescaler_4 :
+			SET_BIT(TWSR , TWPS0) ;
+			break;
+		case I2C_Prescaler_16 :
+			SET_BIT(TWSR , TWPS1) ;
+			break;
+		case I2C_Prescaler_64 :
+			SET_BIT(TWSR , TWPS0) ;
+			SET_BIT(TWSR , TWPS1) ;
+			break;
+		default:
+			break;
+	}
+	
+	//Enable Ack
+	SET_BIT(TWCR , TWEA) ;
+	
+}
+
+/* Send Start condition */
+void i2c_voidStart(void)
+{
+	SET_BIT(TWCR , TWINT) ;			//Clear flag
+	SET_BIT(TWCR , TWSTA) ;			//Send start
+	SET_BIT(TWCR , TWEN)  ;			//Enable I2C
+	
+	while( !(TWCR & (1<<TWINT) ) ){};				//Wait for TWINT Flag set
+}
+
+/* Send Stop condition */
+void i2c_voidStop(void)
+{
+	SET_BIT(TWCR , TWINT) ;			//Clear flag
+	SET_BIT(TWCR , TWSTO) ;			//Send stop
+	SET_BIT(TWCR , TWEN)  ;			//Enable I2C
+	
+	while( !(TWCR & (1<<TWINT) ) ){};				//Wait for TWINT Flag set
+}
+
+/* Send data */
+void i2c_voidSend_Data(u8 data)
+{
+	TWDR = data ;
+	
+	SET_BIT(TWCR , TWINT) ;			//Clear flag
+	SET_BIT(TWCR , TWEN)  ;			//Enable I2C
+		
+	while( !(TWCR & (1<<TWINT) ) ){};				//Wait for TWINT Flag set
+}
+
+/* Check Acknowledge Bit */
+u8 i2c_u8Read_Ack(void)
+{
+	SET_BIT(TWCR , TWINT) ;			//Clear flag
+	SET_BIT(TWCR , TWEN)  ;			//Enable I2C
+	SET_BIT(TWCR , TWEA)  ;			//Enable Ack
+		
+	while( !(TWCR & (1<<TWINT) ) ){};				//Wait for TWINT Flag set
+		
+	return TWDR ;
+}
+
+u8 i2c_u8Read_NACK(void)
+{
+	SET_BIT(TWCR , TWINT) ;			//Clear flag
+	SET_BIT(TWCR , TWEN)  ;			//Enable I2C
+	
+	while( !(TWCR & (1<<TWINT) ) ){};				//Wait for TWINT Flag set
+	
+	return TWDR ;
+}
+
+u8 i2c_u8get_Status(void)
+{
+	u8 status;
+	status = TWSR & 0xF8;
+	return status;
+}
---
+++ Protocols/i2c protocol/Master/I2C.h
@@ -0,0 +1,84 @@
+﻿/*
+ * I2C.h
+ *
+ * Created: 12/04/2021 04:24:37 PM
+ *  Author: Salma
+ */ 
+
+#include "STD_TYPES.h"
+
+#ifndef I2C_H_
+#define I2C_H_
+
+	/* Registers Definition */
+	#define TWDR	*((volatile u8*)0x23)
+	#define TWAR	*((volatile u8*)0x22)
+	#define TWCR	*((volatile u8*)0x56)
+	#define TWSR	*((volatile u8*)0x21)
+	#define TWBR	*((volatile u8*)0x20)
+
+	/* TWCR bits Macros */
+	#define TWINT		7
+	#define TWEA		6
+	#define TWSTA		5
+	#define TWSTO		4
+	#define TWWC		3
+	#define TWEN		2
+	#define TWIE		0
+
+	/* TWSR bits Macros */
+	#define TWPS0		0
+	#define TWPS1		1
+	
+	/* Acknowledge Macros */
+	#define TW_MR_Data_Ack	0x50
+	#define TW_MR_Sla_Ack	0x40
+
+	/*********************** PRESCALER *************************/
+	typedef enum
+	{
+		I2C_Prescaler_1,
+		I2C_Prescaler_4,
+		I2C_Prescaler_16,
+		I2C_Prescaler_64
+		} I2C_PRESCALER ;
+	/***********************************************************/
+	
+	/******************** PUBPLIC FUNCTIONS ********************/
+	/*Description : Initialize I2C in Master Mode
+	  Parameters  : void
+	  return type : void*/
+	void i2c_voidInit(I2C_PRESCALER pre) ;
+	
+	/*Description : Send Start condition in Master Mode
+	  Parameters  : void
+	  return type : void*/
+	void i2c_voidStart(void) ;
+	
+	/*Description : Send Stop condition in Master Mode
+	  Parameters  : void
+	  return type : void*/
+	void i2c_voidStop(void) ;
+	
+	/*Description : Send data
+	  Parameters  : 8-bit data
+	  return type : void*/
+	void i2c_voidSend_Data(u8 data) ;
+	
+	/*Description : Read Acknowledge Bit
+	  Parameters  : void
+	  return type : 8-bit*/
+	u8	 i2c_u8Read_Ack(void) ;
+
+	/*Description : Read NACK
+	  Parameters  : void
+	  return type : 8-bit*/
+	u8 i2c_u8Read_NACK(void) ;
+
+	/*Description : Get status
+	  Parameters  : void
+	  return type : 8-bit*/
+	u8 i2c_u8get_Status(void) ;
+
+
+#endif /* I2C_H_ */
