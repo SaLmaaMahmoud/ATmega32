746c020d229094774c5174b9d375ed116d08d853
Salma Mahmoud <77031899+SaLmaaMahmoud@users.noreply.github.com>
2021-08-12 03:30:55 +0200

    Update TMR.h

--- MCAL/Timer/TMR.h
+++ MCAL/Timer/TMR.h
@@ -82,7 +82,8 @@ typedef enum{
 /********************** Timer0 Functions ***********************/
 void TMR_voidInitTimer0(prescallar pres , State interupt , Modes Mode) ;
 void TMR_voidSetTimer0Count(u8 value);
-u8 TMR_u16GetTimer0Count(void);/**************************************************************/
+u8 TMR_u16GetTimer0Count(void);
+/**************************************************************/
 
 
 /********************** Timer1 Functions ***********************/
@@ -97,6 +98,5 @@ void TMR_voidInitTimer2(prescallar pres , State interupt , Modes Mode) ;
 void TMR_voidSetTimer2Count(u8 value);
 u8 TMR_u16GetTimer2Count(void);
 /**************************************************************/
-
 
 #endif /* TMR_H_ */
