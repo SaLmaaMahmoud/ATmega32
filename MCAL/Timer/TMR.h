5d5038c6aacc29a9ff8ef176356029b8f23e0d3b
Salma Mahmoud <77031899+SaLmaaMahmoud@users.noreply.github.com>
2021-08-13 02:27:58 +0200

    Update TMR.h

--- MCAL/Timer/TMR.h
+++ MCAL/Timer/TMR.h
@@ -10,102 +10,111 @@
 #define TMR_H_
 
 
-/********************** Registers Definition **********************/
-#define TCCR_0		TCCR0
-#define TCNT_0		TCNT0
-#define OCR_0 		OCR0
-#define TIMSK_		TIMSK
-#define TIFR_		TIFR
+	/********************** Registers Definition **********************/
+	/******************** TIMER_0 Definition ********************/
+	#define TCCR_0		TCCR0
+	#define TCNT_0		TCNT0
+	#define OCR_0 		OCR0
+	#define TIMSK_		TIMSK
+	#define TIFR_		TIFR
+	/************************************************************/
+	/******************** TIMER_1 Definition ********************/
+	#define TCCR_1A		TCCR1A
+	#define TCCR_1B		TCCR1B
+	#define TCNT_1		TCNT1
+	/************************************************************/
+	/******************** TIMER_2 Definition ********************/
+	#define TCCR_2		TCCR2
+	#define TCNT_2		TCNT2
+	/************************************************************/
+	/******************************************************************/
 
-#define TCCR_1A		TCCR1A
-#define TCCR_1B		TCCR1B
-#define TCNT_1		TCNT1
+	/************************ Registers PINS *************************/
+	/********************  TIMSK Pins ********************/
+	#define TIMSK_TOIE0		0
+	#define TIMSK_OCIE0		1
+	#define TIMSK_TOIE1		2
+	#define TIMSK_OCIE1B		3
+	#define TIMSK_OCIE1A		4
+	#define TIMSK_TOIE2		6
+	#define TIMSK_OCIE2		7
+	/*****************************************************/
+	/********************  TIFR Pins ********************/
+	#define TIFR_TOV0		0
+	#define TIFR_OCF0		1
+	#define TIFR_TOV1		2
+	#define TIFR_OCF1B		3
+	#define TIFR_OCF1A		4
+	/*****************************************************/
+	/******************** TCCR0 Pins ********************/
+	#define CS00			0
+	#define CS01			1
+	#define CS02			2
+	#define WGM01			3
+	#define WGM00			6
+	/*****************************************************/
+	/******************** TCCR1B Pins ********************/
+	#define CS10			0
+	#define CS11			1
+	#define CS12			2
+	/*****************************************************/
+	/******************** TCCR2 Pins ********************/
+	#define CS20			0
+	#define CS21			1
+	#define CS22			2
+	#define WGM21			3
+	#define WGM20			6
+	/*****************************************************/
+	/****************************************************************/
 
-#define TCCR_2		TCCR2
-#define TCNT_2		TCNT2
-/******************************************************************/
+	/********************** Prescaller Modes ***********************/
+	typedef enum {
+		prescallar_1	,
+		prescallar_8	,
+		prescallar_64	,
+		prescallar_256	,
+		prescallar_1024	,
+		prescallar_FallinEdge ,
+		prescallar_RisinEdge ,
+	} prescallar ;
+	/****************************************************************/
 
-/************************ Registers PINS *************************/
-#define TIMSK_TOIE0		0
-#define TIMSK_OCIE0		1
-#define TIMSK_TOIE1		2
-#define TIMSK_OCIE1B	3
-#define TIMSK_OCIE1A	4
-#define TIMSK_TOIE2		6
-#define TIMSK_OCIE2		7
+	/************************ Timer Modes *************************/
+	typedef enum{
+		Normal ,
+		PWM ,
+		CTC ,
+		Fast_PWM
+	} Modes ;
+	/**************************************************************/
 
-
-#define TIFR_TOV0		0
-#define TIFR_OCF0		1
-#define TIFR_TOV1		2
-#define TIFR_OCF1B		3
-#define TIFR_OCF1A		4
-
-#define CS00			0
-#define CS01			1
-#define CS02			2
-#define WGM01			3
-#define WGM00			6
-
-#define CS10			0
-#define CS11			1
-#define CS12			2
-
-#define CS20			0
-#define CS21			1
-#define CS22			2
-#define WGM21			3
-#define WGM20			6
-/****************************************************************/
-
-/********************** Prescaller Modes ***********************/
-typedef enum {
-	prescallar_1	,
-	prescallar_8	,
-	prescallar_64	,
-	prescallar_256	,
-	prescallar_1024	,
-	prescallar_FallinEdge ,
-	prescallar_RisinEdge ,
-} prescallar ;
-/****************************************************************/
-
-/************************ Timer Modes *************************/
-typedef enum{
-	Normal ,
-	PWM ,
-	CTC ,
-	Fast_PWM
-} Modes ;
-/**************************************************************/
-
-/********************** Interrupt State ***********************/
-typedef enum{
-	Enable ,
-	Disable ,
-} Interrupt_State ;
-/**************************************************************/
-
-
-/********************** Timer0 Functions ***********************/
-void TMR_voidInitTimer0(prescallar pres , Interrupt_State interupt , Modes Mode) ;
-void TMR_voidSetTimer0Count(u8 value);
-u8 TMR_u16GetTimer0Count(void);
-/**************************************************************/
-
-
-/********************** Timer1 Functions ***********************/
-void TMR_voidInitTimer1(prescallar pres , Interrupt_State interupt , Modes Mode) ;
-void TMR_voidSetTimer1Count(u8 value);
-u8 TMR_u16GetTimer1Count(void);
-/**************************************************************/
+	/********************** Interrupt State ***********************/
+	typedef enum{
+		Enable ,
+		Disable ,
+	} Interrupt_State ;
+	/**************************************************************/
 
 
-/********************** Timer2 Functions ***********************/
-void TMR_voidInitTimer2(prescallar pres , Interrupt_State interupt , Modes Mode) ;
-void TMR_voidSetTimer2Count(u8 value);
-u8 TMR_u16GetTimer2Count(void);
-/**************************************************************/
+	/********************** Timer0 Functions ***********************/
+	void TMR_voidInitTimer0(prescallar pres , Interrupt_State interupt , Modes Mode) ;
+	void TMR_voidSetTimer0Count(u8 value);
+	u8 TMR_u16GetTimer0Count(void);
+	/**************************************************************/
+
+
+	/********************** Timer1 Functions ***********************/
+	void TMR_voidInitTimer1(prescallar pres , Interrupt_State interupt , Modes Mode) ;
+	void TMR_voidSetTimer1Count(u8 value);
+	u8 TMR_u16GetTimer1Count(void);
+	/**************************************************************/
+
+
+	/********************** Timer2 Functions ***********************/
+	void TMR_voidInitTimer2(prescallar pres , Interrupt_State interupt , Modes Mode) ;
+	void TMR_voidSetTimer2Count(u8 value);
+	u8 TMR_u16GetTimer2Count(void);
+	/**************************************************************/
 
 
 #endif /* TMR_H_ */
